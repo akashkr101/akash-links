a02c551a5e5138376128c0c1f2ce5d37f8e41277
akashkr101 <akashkumar.xda@gmail.com>
2026-06-19 17:35:22 +0530

    features added

---
+++ data.js
@@ -0,0 +1,48 @@
+const destinations = [
+
+{
+    name: "Kashmir",
+    description: "Snow-covered mountains, frozen rivers and unforgettable adventures.",
+    hero: "images/Kashmir/hero.jpg",
+
+    images: [
+        "images/Kashmir/gallery1.jpg",
+        "images/Kashmir/gallery2.jpg",
+        "images/Kashmir/gallery3.jpg",
+        "images/Kashmir/gallery4.jpg",
+        "images/Kashmir/gallery5.jpg",
+        "images/Kashmir/gallery6.jpg",
+        "images/Kashmir/gallery7.jpg"
+    ],
+
+    videos: [
+        "videos/Kashmir/reel1.mp4"
+    ]
+},
+
+{
+    name: "Nainital",
+    description: "Beautiful lakes and peaceful mountain views.",
+    hero: "images/Nainital/hero.jpg",
+
+    images: [
+        "images/Nainital/gallery1.jpg",
+        "images/Nainital/gallery2.jpg"
+    ],
+
+    videos: []
+},
+
+{
+    name: "Noida",
+    description: "Urban life and city exploration.",
+    hero: "images/Noida/hero.jpg",
+
+    images: [
+        "images/Noida/gallery1.jpg"
+    ],
+
+    videos: []
+}
+
+];
